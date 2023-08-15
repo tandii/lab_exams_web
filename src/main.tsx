f7a46500cd3c56804b4baa842c01cfabae8834a4
Tandi Coelho <tandiccoelho@gmail.com>
2023-08-15 00:18:09 -0300

    feat: configure TanStack provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,18 @@ import { App } from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './global.css'
 import { ToastProvider } from './context/ToastContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ToastProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ToastProvider>
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ToastProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
