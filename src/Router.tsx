d251e34c4ebb1b40800045e5be52bf8e5fe26de0
Tandi Coelho <tandiccoelho@gmail.com>
2023-08-15 00:11:13 -0300

    feat: add new paths
    add new path to Campaign page and Schedule page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
+import { Campaign } from './pages/Campaign'
+import { Schedule } from './pages/Schedule'
 
 export function Router() {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/campaign' element={<Campaign />} />
+            <Route path='/schedule' element={<Schedule />} />
         </Routes>
     )
 }
