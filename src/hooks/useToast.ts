d598a1d85b3869a83a40fb8281444521b939bceb
Tandi Coelho <tandiccoelho@gmail.com>
2023-08-24 13:37:31 -0300

    feat: delete toast context and your imports

--- src/hooks/useToast.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { ToastContext } from "../context/ToastContext";
-
-export function useToast() {
-    const toast = useContext(ToastContext)
-
-    return toast
-}
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './global.css'
-import { ToastProvider } from './context/ToastContext'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
@@ -11,11 +10,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ToastProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ToastProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 )
